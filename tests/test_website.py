fab1296fa35a011fdf601f3fca74ada3f15df8e2
Scott Eppler <seppler@presidio.com>
2024-09-16 10:51:00 -0500

    Fix linting issues

--- tests/test_website.py
+++ tests/test_website.py
@@ -6,9 +6,10 @@ app_path = os.path.dirname(base_path)
 sys.path.append(app_path)
 from app import app
 
+
 class TestHomepage:
     client = app.test_client()
-    response = client.get('/')
+    response = client.get("/")
 
     def test_response_code(self):
         assert self.response.status_code == 200
@@ -16,14 +17,16 @@ class TestHomepage:
     def test_early_reg(self):
         html_line = f'<h2>Early Registration Ends <font color="red">{os.environ.get("EARLY_REG_DATE")}</font></h2>'
         assert html_line.encode() in self.response.data
-    
+
     def test_reg_close(self):
         html_line = f'<h2>Registration Closes <font color="red">{os.environ.get("REG_CLOSE_DATE")}</font></h2>'
         assert html_line.encode() in self.response.data
 
     def test_contact_email(self):
-        html_line = f'You can contact us at <a href="mailto:{os.environ.get("CONTACT_EMAIL")}">{os.environ.get("CONTACT_EMAIL")}</a> if you have questions or issues.'
+        contact_email = os.environ.get("CONTACT_EMAIL")
+        html_line = f'You can contact us at <a href="mailto:{contact_email}">{contact_email}</a> if you have questions or issues.'
         assert html_line.encode() in self.response.data
+
 
 if __name__ == "__main__":
     homepage = TestHomepage()
