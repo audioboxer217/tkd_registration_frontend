309f3b1589232342c24976dfb41f02b3756151e0
Scott Eppler <audioboxer217@gmail.com>
2025-07-07 22:40:13 -0500

    Fix 'upload' API page

--- app.py
+++ app.py
@@ -676,15 +676,17 @@ def schedule_details():
 
 @app.route("/api/upload/<string:resource>", methods=["GET"])
 def upload_form(resource):
-    return render_template(
-        "upload.html",
-        title=f"Upload {resource.capitalize()}",
-        competition_name=os.getenv("COMPETITION_NAME"),
-        favicon_url=url_for("static", filename=get_s3_file(app.config["mediaBucket"], "favicon.png")),
-        event_city=os.getenv("EVENT_CITY"),
-        button_style=os.getenv("BUTTON_STYLE", "btn-primary"),
-        resource=resource,
-    )
+    page_params = {"resource": resource}
+    return render_base("upload.html", **page_params)
+    # return render_template(
+    #     "upload.html",
+    #     title=f"Upload {resource.capitalize()}",
+    #     competition_name=os.getenv("COMPETITION_NAME"),
+    #     favicon_url=url_for("static", filename=get_s3_file(app.config["mediaBucket"], "favicon.png")),
+    #     event_city=os.getenv("EVENT_CITY"),
+    #     button_style=os.getenv("BUTTON_STYLE", "btn-primary"),
+    #     resource=resource,
+    # )
 
 
 @app.route("/api/upload/<string:resource>", methods=["POST"])
